31f3b04fe5a8443fb4226612a50d3d37f1c14db2
Michael <mjfalahee@gmail.com>
2022-02-24 13:39:51 -0800

    test push

---
+++ my-app/src/Views/landingpage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function landingPage() {
+    // this will be the landing page
+    return (
+        <div>
+            <h1>Opposite Sides</h1>
+        </div>
+    )
+}
