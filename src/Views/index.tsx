5e8c89844c8609207d6cc94a4b662cbaee071b1a
mjfalahee <mjfalahee@gmail.com>
2022-06-27 14:25:16 -0700

    cleanup

--- src/Views/index.tsx
+++ src/Views/index.tsx
@@ -2,4 +2,3 @@ export { default as AboutPage } from './AboutPage';
 export { default as ErrorPage } from './ErrorPage';
 export { default as LandingPage } from './LandingPage';
 export { default as MainView } from './MainView';
-export { default as TestView } from './TestView';
